6983e717ce22b8f5297d1cd25e711cfa7ef7180f
Michael Hill <hillm5@jacks.sfasu.edu>
2022-10-04 22:13:37 -0500

    Step 7

---
+++ Players.java
@@ -0,0 +1,33 @@
+import cs331Helper.cs331TicTacToeBoard;
+import cs331Helper.cs331TicTacToeController;
+import cs331Helper.cs331TicTacToePlayer;
+import cs331Helper.cs331TicTacToeSquare;
+
+public class Players implements cs331TicTacToePlayer {
+
+    // member variables
+    private String symbol;
+    private cs331TicTacToeBoard board;
+    private cs331TicTacToeController controller;
+
+    // parameterized constructor
+    Players(cs331TicTacToeBoard inBoard, String inSymbol, cs331TicTacToeController inController) {
+        board = inBoard;
+        symbol = inSymbol;
+        controller = inController;
+    }
+
+    public void selectSquare(int row, int col) {
+        cs331TicTacToeSquare square = board.squareAt(row, col);
+        square.markSquare(symbol);
+        board.clearHighlights();
+        controller.setControllerMessage("Player " + symbol + " finsihed their turn");
+        controller.finishedTurn();
+
+    }
+
+    public String getSymbol() {
+        return this.symbol;
+    }
+
+}
--- myTicTacToe.java
+++ myTicTacToe.java
@@ -1,4 +1,6 @@
+import javafx.scene.paint.Color;
 import java.lang.ModuleLayer.Controller;
+import java.util.ResourceBundle.Control;
 
 import cs331Helper.cs331TicTacToeBoard;
 import cs331Helper.cs331TicTacToeGame;
@@ -9,19 +11,30 @@ public class myTicTacToe implements cs331TicTacToeGame {
     cs331TicTacToeBoard board = new cs331TicTacToeBoard();
     cs331TicTacToeController controller = new cs331TicTacToeController();
 
-    public void invalidSquareChosen(int row, int col) {
+    myTicTacToe() {
+        controller.setControllerMessage("Select a square to start playing!");
+        controller.addPlayer(new Players(board, "X", controller));
+        controller.addPlayer(new Players(board, "O", controller));
+    }
 
+    public void invalidSquareChosen(int row, int col) {
+        controller.setControllerMessage("Square chosen has already been taken. Try again.");
+        board.squareAt(row, col).flashColor(Color.CRIMSON);
     }
 
     public void playerWins() {
-
+        controller.setControllerMessage("Player " + ((Players) controller.getWinningPlayer()).getSymbol() + " wins!");
     }
 
     public void restartGame() {
+        board.clearSymbols();
+        controller.playAgain();
+        controller.setControllerMessage("Select a square to start playing!");
 
     }
 
     public void noWinner() {
+        controller.setControllerMessage("It's a tie.");
 
     }
 }
